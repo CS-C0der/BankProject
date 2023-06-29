d18f38b4eec809025d9a825bbab4f72a216a698d
Chris <121610542+CS-C0der@users.noreply.github.com>
2023-06-29 15:00:11 +0200

    clean up

--- src/Controller.java
+++ src/Controller.java
@@ -6,29 +6,24 @@ import java.util.ArrayList;
 
 public class Controller {
 
-
     public static void main(String[] args) {
 
         ArrayList <Bank> Datenbank = new ArrayList<>();
 
         String eingehenderString = "Testbank";
 
-
         Bank Sparkasse = new Bank("Sparkasse");
         Datenbank.add(Sparkasse);
 
-        Bank Volksbank = new Bank("Volksbank");
-        Datenbank.add(Volksbank);
+        if (Utility.checkIfNotUsed(eingehenderString, Datenbank)) {
+            Bank Volksbank = new Bank(eingehenderString);
+            Datenbank.add(Volksbank);
+        }
 
-        System.out.println(Utility.checkIfNotUsed(eingehenderString, Datenbank));
 
-
-        /*
-        System.out.println(Sparkasse.nameDerBank);
         System.out.println(Datenbank.get(0).nameDerBank);
         System.out.println(Datenbank.get(1).nameDerBank);
 
-        System.out.println(Datenbank.get(1).nameDerBank);
-        */
+
     }
 }
