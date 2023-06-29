1cef01929d7a127ac54dddc815605d86192c59c7
Chris <121610542+CS-C0der@users.noreply.github.com>
2023-06-29 14:55:22 +0200

    create Database of Arraylist type Creat Bank objects in main method and print names

--- lib/Utility.java
+++ lib/Utility.java
@@ -1,14 +1,31 @@
 package lib;
 
+import java.util.ArrayList;
+
 public class Utility {
 
     private Utility() {
 
     };
 
-    static boolean checkIfNotUsed (String first, String second) {
+    static public boolean checkIfNotUsed (String first, String second) {
         if (first == second) {
             return false;
+        }
+        return true;
+    }
+
+    static public boolean checkIfNotUsed (String first, ArrayList<Bank> bank_) {
+
+        System.out.println(bank_);
+
+        for (int i = 0; i < bank_.size(); i++) {
+
+            String second = bank_.get(i).nameDerBank;
+
+            if (first == second) {
+                return false;
+            }
         }
         return true;
     }
--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,7 @@
 import lib.Bank;
+import lib.Utility;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Controller {
@@ -9,6 +11,8 @@ public class Controller {
 
         ArrayList <Bank> Datenbank = new ArrayList<>();
 
+        String eingehenderString = "Testbank";
+
 
         Bank Sparkasse = new Bank("Sparkasse");
         Datenbank.add(Sparkasse);
@@ -16,8 +20,15 @@ public class Controller {
         Bank Volksbank = new Bank("Volksbank");
         Datenbank.add(Volksbank);
 
+        System.out.println(Utility.checkIfNotUsed(eingehenderString, Datenbank));
+
+
+        /*
         System.out.println(Sparkasse.nameDerBank);
         System.out.println(Datenbank.get(0).nameDerBank);
         System.out.println(Datenbank.get(1).nameDerBank);
+
+        System.out.println(Datenbank.get(1).nameDerBank);
+        */
     }
 }
