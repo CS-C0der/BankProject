29299806af2a852695c14153a4661f1b0115aa5e
Chris <121610542+CS-C0der@users.noreply.github.com>
2023-06-29 14:18:04 +0200

    first class with name

---
+++ lib/Bank.java
@@ -0,0 +1,20 @@
+package lib;
+
+public class Bank {
+
+    public String nameDerBank;
+
+    public Bank(String nameDerBank_) {
+        this.nameDerBank = nameDerBank_;
+    }
+
+
+    public String getNameDerBank() {
+        return nameDerBank;
+    }
+
+    public void setNameDerBank(String nameDerBank) {
+        this.nameDerBank = nameDerBank;
+    }
+
+}
---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+import lib.Bank;
+
+public class Controller {
+
+
+    public static void main(String[] args) {
+
+        Bank Sparkasse = new Bank("Sparkasse");
+
+
+        System.out.println(Sparkasse.nameDerBank);
+    }
+}
